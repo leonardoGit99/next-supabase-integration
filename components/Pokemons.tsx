ebacaf6b598474304bb52eb4b808ce092e5f3866
Leonardo Fuentes Claros <leonardofuentesclaros@gmail.com>
2026-03-22 17:36:57 -0400

    supabase nexjs integration

---
+++ components/Pokemons.tsx
@@ -0,0 +1,8 @@
+import { createClient } from '@/lib/supabase/server'
+
+export default async function Pokemons() {
+  const supabase = await createClient()
+  const { data } = await supabase.from('Pokemon').select()
+
+  return <pre>{JSON.stringify(data, null, 2)}</pre>
+}
